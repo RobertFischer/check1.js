296a19e629a6e9eb5092997bed53fea403950cce
Robert Fischer <rfischer@axialexchange.com>
2016-01-14 16:36:09 -0500

    1.0.1 version: uses user-agent to force fresh checks after an upgrade

--- check1.js
+++ check1.js
@@ -1,12 +1,14 @@
-(function(global) {
+(function() {
+  var localWindow = window;
+  var localDocument = document;
+  var prefix = "Check1\t" + localWindow.navigator.userAgent + "\t";
 
   // Detect if we have local storage, or if we have to use a cookie
   var storage;
   try {
-    var string = "Check1";
+    var string = prefix;
     storage = localStorage;
     storage.setItem(string, string);
-    storage.removeItem(string);
   } catch(e) {
     storage = null;
   }
@@ -18,7 +20,7 @@
     }
     :
     function getCookieValue(name) {
-      var result = document.cookie.match('(^|;)\\s*' + encodeURIComponent(name) + '\\s*=\\s*([^;]+)');
+      var result = localDocument.cookie.match('(^|;)\\s*' + encodeURIComponent(name) + '\\s*=\\s*([^;]+)');
       return result ? decodeURIComponent(result.pop()) : null;
     }
   ;
@@ -29,7 +31,7 @@
     }
     :
     function setCookieValue(name, value) {
-      document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+      localDocument.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
     }
   ;
 
@@ -38,13 +40,13 @@
   //   src = source of JS file to load if we need to check (may be `null`)
   //   resultCheck = the callback that performs the result check
   //   resultCallback = function passed the result of the script in any case
-  global.Check1 = function(key, src, resultCheck, resultCallback) {
+  localWindow.Check1 = function(key, src, resultCheck, resultCallback) {
     if(!key) throw "Check1 requires a first argument, which is the key where we should store the results";
     if(!resultCheck) throw "Check1 requires a third argument, which is the callback to execute if a check is needed";
     if(!resultCallback) throw "Check1 requires a fourth argument, which is the function that will get the result";
 
     // Prepend "Check1\t" to the key, just for good measure
-    key = "Check1\t" + key;
+    key = prefix + key;
 
     // If we have a previous value, just feed that into the result callback and be done.
     var previousValue = getValue(key);
@@ -63,15 +65,14 @@
     // If necessary, load JS (eg: Modernizr, mobile-detect.js) and then call resultCheck/resultCallback
     if(src) {
       // Don't worry, multiple loads of same src will use browser-cached result
-      var script = document.createElement("script");
+      var script = localDocument.createElement("script");
       script.src = src;
       script.onload = doResultCheck;
-      document.head.appendChild(script);
+      localDocument.head.appendChild(script);
     } else {
       // No JS to load; just do the result check immediately
       doResultCheck();
     }
 
   };
-
-})(typeof module === 'undefined' ? this : (module.exports={}));
+})();
